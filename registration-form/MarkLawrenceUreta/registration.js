9b3ade28e2adc54bc219c18e526a51e8e3e7a006
marklawrenceureta <marklawrenceureta@gmail.com>
2021-07-05 16:13:15 +0800

    Registration Form By Mark Lawrence Ureta

---
+++ registration-form/MarkLawrenceUreta/registration.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $.get('https://api.first.org/data/v1/teams', function(data, status){
+        var teams = data.data; //result from api
+        var options = '';  
+        //options += '<option value="Select">Select</option>'; 
+
+        for (var i = 0; i < teams.length; i++) {     
+            //construct list of dropdown based from api response
+            options += '<option value="' + teams[i].id + '">' + teams[i].team + '</option>'; 
+        }
+
+        $('#teamList').append(options); //append option with value to dom (dropdown)
+
+    });
+});
+
+function myFunction(){
+    
+    let pass1 = document.forms["myForm"]["passwordControl"].value;
+    let pass2 = document.forms["myForm"]["repasswordControl"].value;
+    let fn = document.forms["myForm"]["firstNameControl"].value;
+    let ln = document.forms["myForm"]["lastNameControl"].value;
+    let em = document.forms["myForm"]["emailControl"].value;
+
+    if (pass1 != pass2) {
+        alert("Password does not match!");
+        return false;
+    
+    }else if(fn == ""){
+        alert("First name missing!");
+    
+    }else if(ln == ""){
+        alert("Last name missing!");
+    
+    }else if(em == ""){
+        alert("Email is missing!");
+    
+    }
+    
+    else {
+      	localStorage.setItem(
+          "First Name",
+          document.getElementById("firstNameControl").value
+        );
+        localStorage.setItem(
+          "Last Name",
+          document.getElementById("lastNameControl").value
+        );
+        localStorage.setItem(
+          "Email",
+          document.getElementById("emailControl").value
+        );
+        localStorage.setItem(
+          "Team List",
+          document.getElementById("teamList").value
+        );
+        localStorage.setItem(
+          "Password",
+          document.getElementById("passwordControl").value
+        );
+        
+        alert("Registered Successfully!");
+        ;
+    }
+}
+
+
