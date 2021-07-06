2a01e5681685bb259e7020e490171e14ab472abf
Ronaldo Hernandez Jr <ronaldo.hernandez.jr@ibm.com>
2021-07-06 13:36:19 +0800

    Eto na Country City SIYAN

--- registration-form/cianbryann/registration.js
+++ registration-form/cianbryann/registration.js
@@ -1,15 +1,60 @@
-$(document).ready(function(){
-    $.get('https://api.first.org/data/v1/teams', function(data, status){
-        var teams = data.data;
-        var options = '';  
-        console.log(status)
-        options += '<option value="Select">Select Team?</option>'; 
-        for (var i = 0; i < teams.length; i++) {     
-            options += '<option value="' + teams[i].id + '">' + teams[i].team + '</option>';
-        }
-        $('#team').append(options);     
+//get teams
+async function getCountry(){
+    const result = await getAPICountry();
+    let options = '<option value="Select">Select Country</option>';  
+    for (i = 0; i < result.length; i++) {     
+        options += '<option value="' + result[i].Code + '">' + result[i].Name + '</option>'; 
+    }
+    document.getElementById('country').innerHTML = options;  
+    console.log(result[1].Name);
+}
+async function getCity(code){
+    var team = document.getElementById("country").value;
+    const result = await getAPICity();
+    let options = '<option value="Select">Select Country</option>';
+    for (i = 0; i < result.length; i++) {     
+        if(result[i].CountryCode == team){
+            options += '<option value="' + result[i].Capital + '">' + result[i].Capital + '</option>';
+        } 
+    }
+    document.getElementById('city').innerHTML = options;  
+    console.log(result[1].CountryCode);
+    console.log(team)
+}
+function getAPICountry(){
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/countries', true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status == 200) {
+                resolve(xhr.response);
+            } else {
+                reject(status);
+            }
+        };
+        xhr.send();
     });
-});
+}
+
+function getAPICity(){
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/citites', true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status == 200) {
+                resolve(xhr.response);
+            } else {
+                reject(status);
+            }
+        };
+        xhr.send();
+    });
+}
+getCountry();
 
 function inputf(){
     var firstname = document.getElementById("FirstName").value;
@@ -17,15 +62,24 @@ function inputf(){
     var email = document.getElementById("Email").value;
     var password1 = document.getElementById("password1").value;
     var password2 = document.getElementById("password2").value;
-    var team = document.getElementById("team").selectedIndex;
-    var team1 = document.getElementById("team").options;
+    var team = document.getElementById("country").selectedIndex;
+    var team1 = document.getElementById("country").options;
+    var team2 = document.getElementById("city").selectedIndex;
+    var team22 = document.getElementById("city").options;
     errorCount = 0;
-    var option = "";
+    var option1 = "";
+    var option2 = "";
 
     if(team1[team].index == 0){
         errorCount = errorCount + 1;
     }else{
-        option = team1[team].text;
+        option1 = team1[team].text;
+    }
+    
+    if(team22[team2].index == 0){
+        errorCount = errorCount + 1;
+    }else{
+        option2 = team22[team2].text;
     }
     
 
@@ -48,7 +102,7 @@ function inputf(){
     }
 
     if(password1.length >= 1){
-        console.log("nicee");
+        console.log("nice");
     }else{
         errorCount = errorCount + 1;
     }
@@ -70,10 +124,8 @@ function inputf(){
         localStorage.setItem("firstName",firstname);
         localStorage.setItem("lastName",lastname);
         localStorage.setItem("email",email);
-        localStorage.setItem("firstName",firstname);
-        localStorage.setItem("lastName",lastname);
-        localStorage.setItem("email",email);
-        localStorage.setItem("team",option)
+        localStorage.setItem("team",option1)
+        localStorage.setItem("team2",option2)
         alert("Nice")
     }else{
         alert("Check Input");
@@ -88,7 +140,7 @@ function clearf(){
     var team = document.getElementById("team").selectedIndex=0;
     var team1 = document.getElementById("team").options="";
 }
-function retrieveff(){
+function retrievef(){
     firstName = localStorage.getItem("firstName");
     lastName = localStorage.getItem("lastName");
     email = localStorage.getItem("email");
