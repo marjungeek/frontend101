e06188f620489f0b8692d6f421db89f7e7c39044
Alyssa Joi De Guzman <alyssa.joi.de.guzman@ibm.com>
2021-07-06 12:39:55 +0800

    Try again

--- stellalequiron/stellalequiron/registration.js
+++ stellalequiron/stellalequiron/registration.js
@@ -11,15 +11,6 @@ $(document).ready(function(){
     });
 });
 
-$(function(){
-    var countries = [];
-
-    $.getJSON('countries.json', function(data){
-        $.each(data.countries, function(i,f){
-            $('#countries').append(options)
-        })
-    })
-})
 
 function passid_validation(passwordControl,mx,my){
     var passwordControl_len = ppasswordControl.value.length;
@@ -31,3 +22,43 @@ function passid_validation(passwordControl,mx,my){
     return true;
 }
 
+function myClear() {
+    alert("Cleared");
+    document.getElementById("registrationForm1").reset();
+    DeleteData()
+    location.reload();
+
+}
+
+async function getCountry(){
+    const Country = await getJSON();
+    let options = '<option value="Select"> - Select - </option>';  //construct list of dropdown based from api response using loops
+    for (var i = 0; i < Country.length; i++) {     
+        options += '<option value="' + Country[i].Name + '">' + Country[i].Name + '</option>'; 
+    }
+    console.log("getJSON");
+    document.getElementById('CList').innerHTML = options;   
+
+    
+}
+
+function getJSON(){
+    return new Promise(function (resolve, reject) {
+        var _HTTPREq = new XMLHttpRequest();
+        _HTTPREq.open('get', 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/countries',true);
+        _HTTPREq.responseType = 'json';
+        _HTTPREq.onload = function () {
+            var status = _HTTPREq.status;
+            if (status == 200) {
+                console.log("Success1!");
+                resolve(_HTTPREq.response);
+            } else {
+                reject(status);
+                console.log("Fail!1");
+            }
+        };
+        _HTTPREq.send();
+    });
+    
+    
+}
