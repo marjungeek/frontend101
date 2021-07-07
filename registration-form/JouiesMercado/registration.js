a6a5e41b238ed4009a2bd728b543604163d79cda
Jouies Mercado <mercadojouies27@gmail.com>
2021-07-07 21:54:32 +0800

    update country and city on .js

--- registration-form/JouiesMercado/registration.js
+++ registration-form/JouiesMercado/registration.js
@@ -1,18 +1,79 @@
-$(document).ready(function(){
-    $.get('https://api.first.org/data/v1/teams', function(data, status) {
-        var teams = data.data; //result from api
-        var options = '';
-        options += '<option value="Select">Select</option>';
+getCountry();
+getCity();
 
-        for (var i = 0; i < teams.length; i++) {
-            options += '<option value="' + teams[i].id + '">' + teams[i].team + '</option>'; 
-            //construct list of dropdown based from api response
+async function getTeams() {
+    const result = await getAPI('get', 'https://api.first.org/data/v1/teams');
+    console.log('teams => ', result);
+    const teams = result.data; //result form api
+    const options = constructDropDown(teams, 'team');
+    document.getElementById('teamL').innerHTML = options;
+}
+
+//reusable function
+function constructDropDown(data, type) {
+    let options = '<option value="">Select</option>';
+    //construct list pf dropdown based from api response using loops
+    for (var i = 0; i < data.length; i++) {
+        let value;
+        let id;
+        if(type == 'team') {
+            value = data[i].team;
+            id = data[i].id;
+        }else{
+            value = data[i].Name;
+            id = data[i].Code;
         }
+        options += '<option value="' + id + '">' + value + '</option>';
+    }
+    return options;
+}
 
-        $('#team').append(options);
-        //append option with value to dom (dropdown)
+//reusable function
+function getAPI(method, endpoint) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, endpoint, true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status == 200) {
+                resolve(xhr.response);
+            }else {
+                reject(status);
+            }
+        };
+        xhr.send();
     });
-});
+}
+
+async function getCountry() {
+    const result = await getAPI('get', 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/countries');
+    console.log('countries => ', result);
+    //const countries = result.data;
+    const option = constructDropDown(result, 'country');
+    document.getElementById('countryList').innerHTML = option;
+}
+
+function getCity(){
+    return getAPI('get', 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/citites');
+}
+
+async function getCapital() {
+    const countryCode = document.getElementById('countryList').value;
+    if(countryCode == '') {
+        alert('Please select your country');
+        document.getElementById('city').value = '';
+    }
+
+    const cities = await getCity();
+
+    for(var i = 0; i < cities.length; i++) {
+        if(countryCode == cities[i].CountryCode) {
+            console.log(countryCode);
+            document.getElementById('city').value = cities[i].Capital;
+        }
+    }
+}
 
 //clear button function
 $(document).ready(function(){
@@ -20,7 +81,10 @@ $(document).ready(function(){
         $('input[type="text"]').val('');
         $('input[type="email"]').val('');
         $('input[type="password"]').val('');
-        $('select[id="team"]').val('');
+        $('select[id="teamL"]').val('');
+        $('select[id="country"]').val('');
+        $('select[id="city"]').val('');
+        $('select[id="gender"]').val('');
         //$('.regform').val('');
         });
     });
