7fdac04a9b8cb9015e27ab0062e1dc6e0b809a1f
paulopez <84214454+elle1114@users.noreply.github.com>
2021-07-07 15:44:01 +0800

    idk

---
+++ registration-form/paulalopez/registration.class.js
@@ -0,0 +1,23 @@
+//--for the reusable functions--
+
+
+// ---for the lists--
+class teamList {
+    constructor(team) {
+        this.teams = team;
+    }
+    get teamList() {
+        return//idk
+    }
+    }
+}
+class countryList {
+    constructor(country) {
+        this.countries = country;
+    }
+}
+class city {
+    constructor() {
+        
+    }
+}
