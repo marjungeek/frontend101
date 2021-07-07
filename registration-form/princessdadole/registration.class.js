67b1d2e354cd17015b1b050723386c4ee5e1baa6
unknown <micahdadole20@gmail.com>
2021-07-08 06:41:16 +0800

    reg form updated

--- registration-form/princessdadole/registration.class.js
+++ registration-form/princessdadole/registration.class.js
@@ -1,19 +1,57 @@
-class ClassName {
-    constructor() {}
-     
-    
+import { HttpClient } from "./HttpClient.js";
 
+export default class RegForm extends HttpClient {
+  constructor(options) {
+    console.log('RegForm was loaded...');
+    super();
 
-getTeams(){
-    const result = await getAPI('get', 'https://api.first.org/data/v1/teams');
-    console.log('teams => ', result);
-    const teams = result.data; //result from api
-    const options = constructDropDown(teams, 'team');
-    document.getElementById('teamList').innerHTML = options;   
-}
+    // class property - sort of like configuration
+    this.countryUrl = options.countryUrl;
+    this.cityUrl = options.cityUrl;
+    this.teamUrl = options.teamUrl;
 
-//reusable function
-constructDropDown(data, type){
+    this.optCountries = document.getElementById('countryList');
+    this.optTeams = document.getElementById('teamList');
+    this.txtCity = document.getElementById('city');
+
+    this.txtEmail = document.getElementById('email');
+
+    this.btnSubmit = document.getElementById('test-submit');
+  }
+
+  async getCountries() {
+    return await this.getRequest(this.countryUrl);
+  }
+
+  async getCities() {
+    return await this.getRequest(this.cityUrl);
+  }
+
+  async getTeams() {
+    return await this.getRequest(this.teamUrl);
+  }
+
+  loadCountries(data) {
+    const options = this.constructDropDown(data, 'country');
+    this.optCountries.innerHTML = options;
+  }
+
+  loadTeams(data) {
+    const options = this.constructDropDown(data, 'team');
+    this.optTeams.innerHTML = options;
+  }
+
+  loadCity(countryCode, cities) {
+    this.txtCity.value = '';
+
+    for(var i = 0; i < cities.length; i++) {
+      if(countryCode == cities[i].CountryCode) {
+        this.txtCity.value = cities[i].Capital;
+      }
+    }
+  }
+
+  constructDropDown(data, type){
     let options = '<option value="">Select</option>';  
     //construct list of dropdown based from api response using loops
     for (var i = 0; i < data.length; i++) {    
@@ -29,53 +67,13 @@ constructDropDown(data, type){
         options += '<option value="' + id + '">' + value + '</option>'; 
     }
     return options;
+  }
 }
 
-//resuable function
-getAPI(method, endpoint){
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, endpoint, true);
-        xhr.responseType = 'json';
-        xhr.onload = function () {
-            var status = xhr.status;
-            if (status == 200) {
-                resolve(xhr.response);
-            } else {
-                reject(status);
-            }
-        };
-        xhr.send();
-    });
+export function doSomething() {
+  console.log('doing something...');
 }
 
-getCountry(){
-    const result = await getAPI('get', 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/countries');
-    console.log('countries => ', result);
-    const option = constructDropDown(result, 'country');
-    document.getElementById('countryList').innerHTML = option;
-}
-
-getCity(){
-    return getAPI('get', 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/citites');
-}
-
-getCapital(){
-    const countryCode = document.getElementById('countryList').value;
-    if(countryCode == ''){
-        alert('Please select your country');
-        document.getElementById('city').value = '';
-    }
-
-    const cities = await getCity();
-
-    for(var i = 0; i < cities.length; i++){
-       if(countryCode == cities[i].CountryCode){
-           console.log(countryCode);
-           document.getElementById('city').value = cities[i].Capital;
-       }
-    }
-}
-
+export function doSomethingAgain() {
 
 }
