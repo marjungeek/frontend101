9ae3b5e11b5bd0a25f38f142e4894054b20fa8c3
brybry017 <74497172+brybry017@users.noreply.github.com>
2021-07-08 23:41:47 +0800

    Delete registration-form/cianbryann/login/cianbryann directory

--- registration-form/cianbryann/login/cianbryann/login.js
+++
@@ -1,8 +0,0 @@
-import logincontrol from './logincontrol.js';
-let lControlObject = new logincontrol();
-
-lControlObject.submit.addEventListener('click',function(){
-    var userNameI = lControlObject.username.value;
-    var passWordI = lControlObject.password.value;
-    lControlObject.getCredentials(userNameI,passWordI);
-});
