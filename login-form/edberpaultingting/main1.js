54b31ca6e892f6b5569ae07c3e894dd013f0b114
Jefferson A Balsacao <jeff.balsacao@ph.ibm.com>
2021-07-08 15:45:22 +0800

    Login HW Success?

--- login-form/edberpaultingting/main1.js
+++ login-form/edberpaultingting/main1.js
@@ -6,16 +6,16 @@ const loginForm = new LoginForm();
 
 // self-invoking function
 (async function() {
-  loginform.loginButton.addEventListener('click', async function(event) {
+  loginForm.loginButton.addEventListener('click', async function(event) {
     event.preventDefault();
 
     console.log("Clicked!");
 
-    let user = loginForm.username.value;
+    let username = loginForm.username.value;
     let password = loginForm.password.value;
-    //let api = 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login';
+    let api = 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login';
 
-    let response = await loginForm.postRequest('http://localhost:8080/login.html', { user, password });
+    let response = await loginForm.postRequest(api, { username, password });
 
     console.log(response);
   });
