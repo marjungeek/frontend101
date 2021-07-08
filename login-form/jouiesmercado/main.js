a2c7a7eee29aacb1bfaacfcfb51fdc3a7a7ae25f
Jouies Mercado <mercadojouies27@gmail.com>
2021-07-08 21:53:48 +0800

    another one

--- login-form/jouiesmercado/main.js
+++ login-form/jouiesmercado/main.js
@@ -6,31 +6,6 @@ const logForm = new LogForm({
     logURL: 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login'
 });
 
-
-/*(async function() {
-    logForm.txtUName.addEventListener('input', function(event) {
-        event.preventDefault();
-
-        const text = event.target.value;
-
-        if (username == 'ghuser'){
-            console.log('valid user');
-        }else {
-            console.log('invalid user');
-        }
-    });
-
-    logForm.btnSubmit.addEventListener('click', async function(event) {
-        event.preventDefault();
-
-        let username = logForm.txtUName.value;
-        let password = logForm.txtPword.value;
-
-        let response = await logForm.postRequest('http://localhost:8080/login.html', {username, password});
-
-        console.log(response);
-    })
-})*/
 (async function(){
     logForm.btnSubmit.addEventListener('click', async function(event) {
         event.preventDefault();
@@ -39,16 +14,16 @@ const logForm = new LogForm({
         let password = logForm.txtPword.value;
 
 
-    let response = await logForm.postRequest('http://localhost:8080/login.html', {username, password});
+    let response = await logForm.postRequest('https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login', {username, password});
 
     console.log(response);
 
-    if ( username || password == 'ghuser' || 'secret'){
+    if ( username && password == 'ghuser' && 'secret'){
         alert("Login success")
     }else {
         alert("error")
 
-        
+
     }
 })
 });
