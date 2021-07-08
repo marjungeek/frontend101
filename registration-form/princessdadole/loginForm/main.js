799962e1babe0606b426e819bf029d6fb7bf4b29
unknown <micahdadole20@gmail.com>
2021-07-08 14:06:45 +0800

    asdada

--- registration-form/princessdadole/loginForm/main.js
+++ registration-form/princessdadole/loginForm/main.js
@@ -1,1 +1,15 @@
 import LoginForm from "./LoginForm.js";
+console.log("Application Loaded..");
+const loginForm = new LoginForm({
+    url = "https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login"
+})
+
+loginForm.btnSubmit.addEventListener('click',async function(event){
+    event.preventDefault();
+     let user  = loginForm.txtEmail;
+     let password = loginForm.txtPassword;
+
+     let response = await regForm.postRequest('https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login', { user, password });
+
+     console.log(response);
+})
