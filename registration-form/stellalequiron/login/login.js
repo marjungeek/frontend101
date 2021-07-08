e1ac8b338e20fba1636fa2681674ba8110d43fde
Alyssa Joi De Guzman <alyssa.joi.de.guzman@ibm.com>
2021-07-08 12:50:04 +0800

    login trial

--- registration-form/stellalequiron/login/login.js
+++ registration-form/stellalequiron/login/login.js
@@ -1,1 +1,11 @@
 var attempt = 3; //ito ung sa number ng attempts
+// para sa login button
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == ""){
+        alert("Enter Username");
+        return false;
+    }
+    if 
+}
