c2b83cbae795b0d5dea131f54911733f41532d5f
Lanz William Uy <Lanzuy06@gmail.com>
2021-07-09 12:56:07 +0800

    Log-in Form done

--- login-form/Lanzuy/login/login.js
+++ login-form/Lanzuy/login/login.js
@@ -17,7 +17,12 @@ let password = loginForm.txtPassword.value;
 
 let response = await loginForm.postRequest('https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login',{ username, password});
 console.log(response);
-
+ if (username == 'ghuser' && password == 'secret'){
+    
+    document.location.href = 'http://www.google.com';
+ }else{
+     alert("Login Fail");
+ }  
 
 });
 })();
