92a839a2712525e8719e0631328340209860678c
ravenrome <ravenromebarcelos@gmail.com>
2021-07-15 16:17:44 +0800

    TS modified

--- login-form2/login-main.ts
+++ login-form2/login-main.ts
@@ -1,11 +1,10 @@
 const username = document.getElementById('username')! as HTMLInputElement;
 const password = document.getElementById('password')! as HTMLInputElement;
-
 const btnLogIn = document.getElementById('submit')! as HTMLElement;
 
 
-(async function(){
-    btnLogIn.addEventListener('click', async function(event){
+(function(){
+    btnLogIn.addEventListener('click', function(event){
         event.preventDefault();
     
         interface LoginResponse{
