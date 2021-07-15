f3deb897e338d9015d5b737ac6baa39e142af452
ravenrome <ravenromebarcelos@gmail.com>
2021-07-15 17:12:27 +0800

    All modified

---
+++ login-form2/LogInMain.ts
@@ -0,0 +1,27 @@
+import ClassForm from 'LogInClass.js';
+
+console.log("Log In Form Loaded!!!");
+
+const classForm = new ClassForm({
+    LoginAPI: 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login'
+});
+
+(async function(){
+    classForm.btnLogin.addEventListener('click', async function(event){
+        event.preventDefault();
+        let username = classForm.txtUsername.value;
+        let password = classForm.txtPassword.value;
+
+        let response = await classForm.postRequest('https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login', {username, password});
+
+        console.log(response);
+
+        if(response.statusCode == 200){
+            alert("Log In Successful");
+        } else if(username == '' && password == ''){
+            alert("Missing Credentials");
+        }else{
+            alert("Log In Failed");
+        }
+    });
+})();
