b751c5ad1dfd3f32c79f8eb0c369bb52d31e771c
ravenrome <ravenromebarcelos@gmail.com>
2021-07-15 15:57:42 +0800

    TypeScript modified

--- login-form2/login-main.ts
+++ login-form2/login-main.ts
@@ -2,3 +2,26 @@ const username = document.getElementById('username')! as HTMLInputElement;
 const password = document.getElementById('password')! as HTMLInputElement;
 
 const btnLogIn = document.getElementById('submit')! as HTMLElement;
+
+
+(async function(){
+    btnLogIn.addEventListener('click', async function(event){
+        event.preventDefault();
+    
+        interface LoginResponse{
+            statusCode: number;
+            message: string;
+        }
+
+        function sendLogin(username: string, password: string): LoginResponse{
+            return {statusCode: 200, message: "Log In Success"};
+        }
+
+        let response = sendLogin('ghuser', 'secret');
+
+        if(response.statusCode === 200){
+            console.log("Success");
+        }
+
+    });
+})();
