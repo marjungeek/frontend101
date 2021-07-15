1901f3c428aaa9ee4d3bb88633c8ab91f5814576
ravenrome <ravenromebarcelos@gmail.com>
2021-07-15 13:51:45 +0800

    TypeScript in Progress

--- login-form2/login-main.ts
+++ login-form2/login-main.ts
@@ -1,27 +1,5 @@
-import ClassForm from "./LoginClass.ts";
+import {HttpClient} from "./HttpClient";
 
-console.log("Log In Form Loaded")
-
-const classForm = new ClassForm({
-    LoginAPI: 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login'
-});
-
-(async function(){
-    classForm.btnLogIn.addEventListener('click', async function(event){
-        event.preventDefault();
-        let username = classForm.txtUsername.value;
-        let password = classForm.txtPassword.value;
-
-        let response = await classForm.postRequest('https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login', {username, password});
-
-        console.log(response);
-
-        if(response.statusCode == 200){
-            alert("Log In Successful");
-        } else if(username == '' && password == ''){
-            alert("Missing Credentials");
-        }else{
-            alert("Log In Failed");
-        }
-    });
-})();
+class LogIn {
+    
+}
