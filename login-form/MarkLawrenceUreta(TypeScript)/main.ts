62dac550b44c84333ee1020f1c2b0d39ff8ecadf
marklawrenceureta <marklawrenceureta@gmail.com>
2021-07-19 12:18:37 +0800

    created TS files(RAW)

---
+++ login-form/MarkLawrenceUreta(TypeScript)/main.ts
@@ -0,0 +1,38 @@
+import LoginForm from "./LoginForm.js";
+
+console.log("Application was loaded...");
+
+const loginForm = new LoginForm({
+  baseURL:
+    "https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login",
+});
+
+(async function () {
+  loginForm.btnSubmit.addEventListener("click", async function (event) {
+    event.preventDefault();
+
+    let username = loginForm.userName.value;
+    let password = loginForm.password.value;
+
+    let response = await loginForm.postRequest(
+      "https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login",
+      { username, password }
+    );
+    console.log(response);
+    if (response.statusCode == 200) {
+      location.href = "https://www.ibm.com/us-en/";
+    } else {
+      alert("Wrong Username or Password!");
+    }
+  });
+
+  loginForm.btnTwit.addEventListener("click", async function (event) {
+    event.preventDefault();
+    location.href = "https://twitter.com/login?lang=en";
+  });
+
+  loginForm.btnFb.addEventListener("click", async function (event) {
+    event.preventDefault();
+    location.href = "https://www.facebook.com/";
+  });
+})();
