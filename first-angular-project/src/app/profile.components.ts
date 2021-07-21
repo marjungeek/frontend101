445f00a5cb0c5594f0e613fa66fd5fe32984211f
paulopez <84214454+elle1114@users.noreply.github.com>
2021-07-21 19:35:10 +0800

    inc

---
+++ first-angular-project/src/app/profile.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: ,
+  // styleUrls: ['./app.component.css']
+  loadState:
+})
+export class AppComponent {
+  title = 'first-angular-project';
+}
