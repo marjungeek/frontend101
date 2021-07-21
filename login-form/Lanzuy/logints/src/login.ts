7a27a6f499ed2011acd010f015119eefd4a278ed
Jefferson A Balsacao <Lanzuy06@gmail.com>
2021-07-21 15:49:50 +0800

    login typescript pt2

--- login-form/Lanzuy/logints/src/login.ts
+++ login-form/Lanzuy/logints/src/login.ts
@@ -2,7 +2,13 @@ import loginform from "./loginform.js"
 
 console.log('login loaded');
 const loginForm = new loginform({
-    var loginApi: string = 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login'
+    loginApi: 'https://22pnpc80ni.execute-api.ap-southeast-1.amazonaws.com/dev/login',
+    success:function(){
+        console.log('success');
+    },
+    error:function(){
+        console.log('success');
+    }
 });
 loginForm.listener();
 
