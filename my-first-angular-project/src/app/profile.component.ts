78d779cbee0fccac9c6da7897d1d574d544566d0
ravenrome <ravenromebarcelos@gmail.com>
2021-07-22 17:17:42 +0800

    Restore Default

---
+++ my-first-angular-project/src/app/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: ' app-root',
+  templateUrl: './profile.component.html'
+})
+
+export class ProfileComponent {
+  title = 'Exercise #1';
+  loadState = 'loading...';
+}
