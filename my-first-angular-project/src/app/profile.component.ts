e5865a12ec38694d80f6d3be02ba31b18f6a58db
ravenrome <ravenromebarcelos@gmail.com>
2021-07-22 13:03:49 +0800

    All Modified

--- my-first-angular-project/src/app/profile.component.ts
+++ my-first-angular-project/src/app/profile.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: ' app-root',
-  template: ''
+  template: '
+  <p>loadState :</p>
+  <p id ="demo"> {{loading...}} </p>
+  <button type = "button" onclick="loading()"> Click Here </button>'
 })
 
 export class ProfileComponent {
-
+  function loading(){
+    document.getElementById("demo")?.innerHTML = "finished";
+  }
 }
