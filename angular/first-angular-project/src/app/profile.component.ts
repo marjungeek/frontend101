78a5e3a01681fc7c5a89d0d5c4d915da85d04c11
Jefferson A Balsacao <jeff.balsacao@ph.ibm.com>
2021-07-22 12:56:06 +0800

    1st Angular HW Complete

--- angular/first-angular-project/src/app/profile.component.ts
+++ angular/first-angular-project/src/app/profile.component.ts
@@ -3,19 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <p>{{ title }}</p>
-  <p>{{ loadState }}</p>
-  <button name="clickEvent" (click)="someEvent($event)"></button>
-
+  <div class="container">
+  <p>{{ title }}</p><hr/>
+  <p>Load State: {{ loadState }}</p>
+  <button name="clickEvent" (click)="someEvent($event)">Finished</button>
+  <hr/>
+  <p>Type something below!</p>
   <input name="userChangeLoadState" type="text" [(ngModel)]="loadState" >
+  </div>
   <!--
   Error on line 10, I tried to create new profile.module.ts, I think I fixed it??
   Now need to know how to display it on the browser
 -->
   `
 })
-
-
 
 export class ProfileComponent {
   title = 'Profile Component Trial';
