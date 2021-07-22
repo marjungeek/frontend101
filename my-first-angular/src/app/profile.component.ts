e2524b6103f62ac7c064d5912999064644cf7489
GMX\P101G6PH1 <henrich.molina012@gmail.com>
2021-07-22 13:04:22 +0800

    asd

--- my-first-angular/src/app/profile.component.ts
+++ my-first-angular/src/app/profile.component.ts
@@ -1,9 +1,28 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component ({
 selector: 'app-root',
-template:
+template:`
+       <p> {{loadstate}} </p>
+      <input  type='text' (input)="onUserInput($event)" [value]= "loadstate" />
+      <p>peek me</p>
+      <button onclick="">Click</button>
 
-<input type= "text"  [(ngModel)]="name" >
+`
 
-)}
+})
+export class AppComponent{
+title = 'Angular';
+loadstate = 'loading';
+
+onUserInput(event:any){
+   this.loadstate = event.target.value
+//   this.btnsubmit = document.getElementById('submit')
+   }
+}
+
+// const btn  = document.getElementById('submit')
+// btn.addEventListener('click', async function(event)
+
+
+
