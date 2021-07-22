142b77e65b2099b548275f1531ec26e81e2977e8
Jefferson A Balsacao <jeff.balsacao@ph.ibm.com>
2021-07-23 06:02:39 +0800

    2nd Angular Activity Not Done V2

--- angular/second-project/src/app/test-child/test-child.component.ts
+++ angular/second-project/src/app/test-child/test-child.component.ts
@@ -1,19 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test-child',
   templateUrl: './test-child.component.html',
   styleUrls: ['./test-child.component.css']
 })
-export class TestChildComponent implements OnInit {
+export class TestChildComponent implements OnInit {//Ask about what is implements, and OnInit
 
-  //@Input()
+  @Input() feedList: any = [];
 
-  feedList: any = [{cfeedValue: 'Sample Feed'}];
+  @Output() likeEvent = new EventEmitter<number>();
+  likeCounter: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addLikeEvent(IDName: string) {
+    // console.log("addLikeEvent Function Activated"); //For Testing if this function is being called
+    //console.log(typeof IDName);
+    //document.getElementById(IDName)?.setAttribute("style", "display: none;");
+    let eventCounter = this.likeCounter++
+    this.likeEvent.emit(eventCounter);
+  }
+
+  //July 23: Currently stuck in trying to turn the like button to a dislike button
+  //current plan is to use the unique ID of button then do DOM Manipulation from this TS File
+  //see line 23
 }
--- angular/second-project/src/app/test-parent/test-parent.component.ts
+++ angular/second-project/src/app/test-parent/test-parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-parent',
@@ -7,13 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestParentComponent implements OnInit {
 
+  likeCounterParent: number = 0;
+
+  feedList: any = [];
+  forCheck: boolean = false;
+  feedIDCounter: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  submitToFeed(){
-
+  submitToFeed(input: any, domInput: HTMLInputElement){
+    //console.log("submitToFeed Function Activated"); For Testing if this function is being called
+    if(input == "" || input.trim() == ""){
+      this.forCheck = true;
+      domInput.value = '';
+    } else{
+      let tempFeedIDCounter = this.feedIDCounter++
+      let uFeedID: string = "id" + String(tempFeedIDCounter);
+      //console.log(typeof uFeedID);
+      this.feedList.push({cfeedValue: input, feedID: uFeedID});
+      domInput.value = '';
+      this.forCheck = false;
+      console.log(this.feedList);
+    }
   }
 
+  likeEventFromChild(value: number) {
+    this.likeCounterParent = value;
+  }
 }
