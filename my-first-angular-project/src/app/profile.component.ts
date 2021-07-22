2fd5dd00499eb04fa1f9a3bc5680b24e72c9fb10
ravenrome <ravenromebarcelos@gmail.com>
2021-07-22 15:23:15 +0800

    Modified

--- my-first-angular-project/src/app/profile.component.ts
+++ my-first-angular-project/src/app/profile.component.ts
@@ -3,17 +3,18 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: ' app-root',
-  template: '
-    <p>Exercise</p>
-    <p> {{loadState}} </p>
-    <button (click)="(loadState='finished')"> Click Here </button>
-    <br>
-    <br>
-    <input name="user" type="text" [(ngModel)]="loadState" >
-  '
-});
+  templateUrl: 'profile.component.html'
+  // template:'
+  // <p>Exercise</p>
+  // <p> {{loadState}} </p>
+  // <button (click)="(loadState='finished')"> Click Here </button>
+  // <br>
+  // <br>
+  // <input name="user" type="text" [(ngModel)]="loadState">
+  // '
+})
 
 export class ProfileComponent {
   title = 'Exercise #1';
-  loadState = 'loading...'
+  loadState = 'loading...';
 }
