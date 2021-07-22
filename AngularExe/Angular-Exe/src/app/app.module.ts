15c4df3f1fa4096644f48bfd9106af84d619c86d
RRyanRae <rryanrae@gmail.com>
2021-07-22 12:38:19 +0800

    AngularTry

---
+++ AngularExe/Angular-Exe/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ProfileComponentComponent } from './profile-component/profile-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
