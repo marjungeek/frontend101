00846cfcf517bdf147976d629ef93e2479f4b617
ravenrome <ravenromebarcelos@gmail.com>
2021-07-23 06:55:29 +0800

    All Modified

--- html-selector/src/app/app.component.ts
+++ html-selector/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Exercise #1';
-  loadState = 'loading...';
+  title = 'html-selector';
 }
---
+++ html-selector/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ExerciseComponent } from './exercise/exercise.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExerciseComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
