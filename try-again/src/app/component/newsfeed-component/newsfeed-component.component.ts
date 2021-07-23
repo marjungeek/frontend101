97205e2e30de96f59418994339f55242837514d5
Ronaldo Hernandez Jr <ronaldo.hernandez.jr@ibm.com>
2021-07-23 13:26:43 +0800

    Added Total Likes

--- try-again/src/app/component/newsfeed-component/newsfeed-component.component.ts
+++ try-again/src/app/component/newsfeed-component/newsfeed-component.component.ts
@@ -7,25 +7,37 @@ import { Component, OnInit} from '@angular/core';
 })
 export class NewsfeedComponentComponent implements OnInit {
   like: boolean=false;
+  total = 0;
+
   newsfeed = [
-    {Sinulat:"HUHUHUHUHUH",Like:69},
-    {Sinulat:"HUHUHUHUHUsafaskfsakjfjsafkjsafsakjja",Like:59}
+    {Sinulat:"HUHUHUHUHUH",Like:69,Pinindot:false},
+    {Sinulat:"HUHUHUHUHUsafaskfsakjfjsafkjsafsakjja",Like:59,Pinindot:false}
   ]
+
   constructor() { }
 
   ngOnInit(): void {
+    for(let tanga in this.newsfeed){
+      this.total += this.newsfeed[tanga].Like;
+    }
   }
   bago(sulat:string){
-    this.newsfeed.push({Sinulat:sulat,Like:0});
+    this.newsfeed.push({Sinulat:sulat,Like:0,Pinindot:false});
   }
   addLike(vad:number){
-    this.like = !this.like;
-    if(this.like == true){
+    if(this.newsfeed[vad].Pinindot == false){
+      console.log(this.newsfeed[vad].Pinindot);
       this.newsfeed[vad].Like++;
+      this.total++;
+      this.newsfeed[vad].Pinindot = !this.newsfeed[vad].Pinindot;
+      console.log(this.newsfeed[vad].Pinindot);
     }else{
+      console.log(this.newsfeed[vad].Pinindot);
       this.newsfeed[vad].Like--;
+      this.total--;
+      this.newsfeed[vad].Pinindot = !this.newsfeed[vad].Pinindot;
+      console.log(this.newsfeed[vad].Pinindot);
     }
-    console.log(this.newsfeed[vad]);
   }
 
 }
--- try-again/src/app/component/textbox-component/textbox-component.component.ts
+++ try-again/src/app/component/textbox-component/textbox-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
 
 @Component({
   selector: 'app-textbox-component',
@@ -7,6 +7,7 @@ import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 })
 export class TextboxComponentComponent implements OnInit {
   @Output() newSulat = new EventEmitter<string>();
+  @Input() likeCount = 0;
   constructor() { }
 
   ngOnInit(): void {
