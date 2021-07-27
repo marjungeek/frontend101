5c18b5b45ce073f30df191f7bc210ea342e0cc72
marjungeek <marjunvillegas.geek@gmail.com>
2021-07-27 17:51:11 +0800

    dependency injection

---
+++ di-http-client/src/app/form/form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  contentMessage: string;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+
+  }
+  updateMessage(message: HTMLInputElement){
+    this.apiService.updateMessage(message.value);
+  }
+
+}
