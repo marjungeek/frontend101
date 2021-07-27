8d5a5487055fb45542d86c657d3ed5a4f27f47fd
ravenrome <ravenromebarcelos@gmail.com>
2021-07-27 15:08:04 +0800

    2nd demo Project

---
+++ my-second-angular-project/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ my-second-angular-project/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { ButtonComponent } from './button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule { }
