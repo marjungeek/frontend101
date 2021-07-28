812a4d75fffc02a0a0db9d3af0af14f33c628e66
micahdadole20@gmail.com <alyssa.joi.de.guzman@ibm.com>
2021-07-28 17:43:33 +0800

    1

--- angular-essentials/princessdadole/bootstrap-angular-app/src/app/app.module.ts
+++ angular-essentials/princessdadole/bootstrap-angular-app/src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostListComponent } from './post-list/post-list.component';
-import { AppRoutingComponent } from './app-routing/app-routing.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PostListComponent,
-    AppRoutingComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
--- angular-essentials/princessdadole/bootstrap-angular-app/src/app/post/post.component.spec.ts
+++ angular-essentials/princessdadole/bootstrap-angular-app/src/app/post/post.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppRoutingComponent } from './app-routing.component';
+import { PostComponent } from './post.component';
 
-describe('AppRoutingComponent', () => {
-  let component: AppRoutingComponent;
-  let fixture: ComponentFixture<AppRoutingComponent>;
+describe('PostComponent', () => {
+  let component: PostComponent;
+  let fixture: ComponentFixture<PostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AppRoutingComponent ]
+      declarations: [ PostComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppRoutingComponent);
+    fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
