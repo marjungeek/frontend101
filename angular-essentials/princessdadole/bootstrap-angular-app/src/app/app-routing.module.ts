7d0e3d49090d4849ae80acdd0b544aeaf6ea553f
micahdadole20@gmail.com <micahdadole20@gmail.com>
2021-07-28 18:07:48 +0800

    princess update

---
+++ angular-essentials/princessdadole/bootstrap-angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+
+const routes: Routes = [
+  {path: 'post', component: PostListComponent}
+
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
