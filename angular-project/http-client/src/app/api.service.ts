1cff088cb51cf80ea2b06c63acbe77edf0885ebb
Euzell Acain <acaineuzell@gmail.com>
2021-07-28 13:07:53 +0800

    apiservice

---
+++ angular-project/http-client/src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+   private message$ = new BehaviorSubject<string>("Default Message");
+   messageData = this.message$.asObservable();
+
+  constructor() { }
+
+  updateMessage(newMessage: string){
+    this.message$.next(newMessage);//gagamitin para iupdate yung default message
+
+  }
+}
---
+++ angular-project/http-client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NewsComponent } from './news/news.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    NewsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
