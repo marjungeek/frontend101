aceaa23bfc11ae46a9ae810b2d6fd427be03b6df
Jouies Mercado <mercadojouies27@gmail.com>
2021-07-28 17:23:22 +0800

    interface

---
+++ bootstrap-angular-project/src/app/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { PostInterface } from '../post';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  posts!: PostInterface [];
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+    const result = this.postService.getList();
+    result.subscribe(result => {
+      this.posts = result;
+    })
+  }
+
+}
