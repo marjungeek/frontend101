56ddea83650dd11bc5dc84a9836bc2abf5e34bdd
paulopez <84214454+elle1114@users.noreply.github.com>
2021-07-28 16:08:20 +0800

    w/o interface

---
+++ bootstrap-angular-project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostListComponent } from './post-list/post-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
