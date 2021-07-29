3d3b8f2963185306573c49a035d8cc8c99ff6165
ravenrome <ravenromebarcelos@gmail.com>
2021-07-29 12:26:41 +0800

    Services and Routing

--- BootStrap-Angular-App/src/app/app-routing.module.ts
+++ BootStrap-Angular-App/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeaturesComponent } from './features/features.component';
 import { HomeComponent } from './home/home.component';
 import { PostListComponent } from './post-list/post-list.component';
-import { PricingComponent } from './pricing/pricing.component';
 import { PostComponent } from './post/post.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent },
-  { path: 'posts/:id', component: PostComponent },
   { path: 'posts', component: PostListComponent },
+  { path: 'posts/:id', component: PostComponent },
   { path: 'features', component: FeaturesComponent },
   { path: 'pricing', component: PricingComponent }
 ];
