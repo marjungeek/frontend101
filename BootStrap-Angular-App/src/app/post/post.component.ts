4ef6d3a560aab1daa2dd8eadd21c40bf17173c6c
ravenrome <ravenromebarcelos@gmail.com>
2021-07-29 13:34:53 +0800

    Data Modified

--- BootStrap-Angular-App/src/app/post/post.component.ts
+++ BootStrap-Angular-App/src/app/post/post.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PostService } from '../post.service';
-import { Data } from '../data';
+import { Data } from './data';
 
 @Component({
   selector: 'app-post',
