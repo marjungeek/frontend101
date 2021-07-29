183fad0ec20d92a2302a4888cd6f4ffd4550af1b
ravenrome <ravenromebarcelos@gmail.com>
2021-07-29 17:35:30 +0800

    All modified

--- AngularforExercise/src/app/app-routing.module.ts
+++ AngularforExercise/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TeamDescriptionComponent } from './team-description/team-description.component';
+import { TeamsComponent } from './teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'teams', component: TeamsComponent},
+  {path: 'team/:id', component: TeamDescriptionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
